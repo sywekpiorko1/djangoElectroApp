448ea8f8eb0cc1b6e4a7e83525248b81e258db03
sywekpiorko1 <termetal@gmail.com>
2019-10-14 19:59:14 +0100

    create view and template of warhouse items

--- electron_django/warehouse/views.py
+++ electron_django/warehouse/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+def get_items_list(request):
+    results = Item.objects.all()
+    return render(request, "warehouse/items_list.html", {'items': results, 'title': 'Warehouse items'})
