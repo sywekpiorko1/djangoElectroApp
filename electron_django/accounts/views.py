998b44b29f5e6a0f0cf92a8ccde0fc275944c8dd
sywekpiorko1 <termetal@gmail.com>
2019-10-21 22:52:11 +0100

    Add profile page for account with with picture upload, add signal to create profile when new account created

--- electron_django/accounts/views.py
+++ electron_django/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -29,6 +30,6 @@ def register(request):
     
     return render(request, 'accounts/register.html', {'form': form})
 
-
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
--- electron_django/electron_django/urls.py
+++ electron_django/electron_django/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from accounts import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('', include('warehouse.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
